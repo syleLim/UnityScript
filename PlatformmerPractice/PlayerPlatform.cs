f299d845ff7265f4cc9a9f47819c1c37fc2689dc
syleLim <syle6492@gmail.com>
2020-08-07 09:38:38 +0900

    Jump and Ground Check update

--- PlatformmerPractice/PlayerPlatform.cs
+++ PlatformmerPractice/PlayerPlatform.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+*   This script work with anim and material setting,
+*   anim : 15:40 ~ https://www.youtube.com/watch?v=MReoItM8BoI
+*   material : 19:00 ~ https://www.youtube.com/watch?v=MReoItM8BoI
+*/
 public class PlayerPlatform : MonoBehaviour
 {
     /*
@@ -11,20 +16,32 @@ public class PlayerPlatform : MonoBehaviour
     */
     private const float velocity = 10f;
     private const float jumpForce = 2f;
+    public float groundCheckRadius;
+    public int amountOfJumps = 2;
 
     protected Rigidbody2D rb2D;
+    protected Animator anim;
     protected float moveDirection;
+    public Transform groundCheck;
+    public LayerMask WhatIsGround;
+    
     private bool isFacingRight;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
+    private int amountOfJumpsLeft;
+
 
     private void OnEnable() {
         rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         isFacingRight = true;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
@@ -32,10 +49,28 @@ public class PlayerPlatform : MonoBehaviour
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimation();
     }
 
     private void FixedUpdate() {
         applyMovement();
+        CheckSurroundings();
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, WhatIsGround);
+    }
+
+    private void CheckIfJump()
+    {
+        if (isGrounded && rb2D.velocity.y <= 0)
+            amountOfJumpsLeft = amountOfJumps;
+        
+        if (amountOfJumpsLeft <= 0)
+            canJump = false;
+        else
+            canJump = true;
     }
 
     protected void CheckInput()
@@ -44,13 +79,14 @@ public class PlayerPlatform : MonoBehaviour
 
         if (Input.GetButtonDown("jump"))
         {
-
+            Jump();
         }
     }
 
     protected void Jump()
     {
-        rb2D.velocity = new Vector2(rb2D.velocity.x, jumFroce);
+        rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
+        amountOfJumpsLeft--;
     }
 
     protected void CheckMovementDirection()
@@ -59,6 +95,12 @@ public class PlayerPlatform : MonoBehaviour
             Flip();
         else if (!isFacingRight && moveDirection > 0)
             Flip(); 
+        
+        if (rb2D.velocity.x != 0)
+            isWalking = true;
+        else
+            isWalking = false;
+
     }
 
     protected void Flip()
@@ -70,5 +112,13 @@ public class PlayerPlatform : MonoBehaviour
     protected void applyMovement()
     {
         rb2D.velocity = new Vector2(velocity * moveDirection, rb2D.velocity.y);
+    }
+
+    protected void UpdateAnimation()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        //Seting downward animation : 15:40 in https://www.youtube.com/watch?v=MReoItM8BoI
+        anim.SetFloat("yVelocity", rb2D.velocity.y);
     }
 }
