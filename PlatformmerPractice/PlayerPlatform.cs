3faa3579495ea668a5cb03bf48c7470b04dbdb51
syleLim <syle6492@gmail.com>
2020-08-08 14:56:16 +0900

    wall slide update

--- PlatformmerPractice/PlayerPlatform.cs
+++ PlatformmerPractice/PlayerPlatform.cs
@@ -16,18 +16,25 @@ public class PlayerPlatform : MonoBehaviour
     */
     private const float velocity = 10f;
     private const float jumpForce = 2f;
-    public float groundCheckRadius;
     public int amountOfJumps = 2;
+    public float wallSlidingSpeed;
+    public float groundCheckRadius;
+    public float wallCheckDistance;
+    
+    
 
     protected Rigidbody2D rb2D;
     protected Animator anim;
     protected float moveDirection;
     public Transform groundCheck;
+    public Transform wallCheck;
     public LayerMask WhatIsGround;
     
     private bool isFacingRight;
     private bool isWalking;
     private bool isGrounded;
+    private bool isTouchingWall;
+    private bool isWallSliding;
     private bool canJump;
     private int amountOfJumpsLeft;
 
@@ -50,27 +57,21 @@ public class PlayerPlatform : MonoBehaviour
         CheckInput();
         CheckMovementDirection();
         UpdateAnimation();
+        CheckIfJump();
+        CheckIfWallSliding();
+    }
+
+    private void CheckIfWallSliding()
+    {
+        if (isTouchingWall && !isGrounded && rb2D.velocity.y < 0)
+            isWallSliding = true;
+        else
+            isWallSliding = false;
     }
 
     private void FixedUpdate() {
         applyMovement();
         CheckSurroundings();
-    }
-
-    private void CheckSurroundings()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, WhatIsGround);
-    }
-
-    private void CheckIfJump()
-    {
-        if (isGrounded && rb2D.velocity.y <= 0)
-            amountOfJumpsLeft = amountOfJumps;
-        
-        if (amountOfJumpsLeft <= 0)
-            canJump = false;
-        else
-            canJump = true;
     }
 
     protected void CheckInput()
@@ -89,6 +90,29 @@ public class PlayerPlatform : MonoBehaviour
         amountOfJumpsLeft--;
     }
 
+    /*
+    *   For Ground Check. It is called grounding(접지)
+    *   Overlap checks objs in setting layer. 
+    */
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, WhatIsGround);
+        
+        // Check Wall with right direction 
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, WhatIsGround);
+    }
+
+    private void CheckIfJump()
+    {
+        if (isGrounded && rb2D.velocity.y <= 0)
+            amountOfJumpsLeft = amountOfJumps;
+        
+        if (amountOfJumpsLeft <= 0)
+            canJump = false;
+        else
+            canJump = true;
+    }
+
     protected void CheckMovementDirection()
     {
         if (isFacingRight && moveDirection < 0)
@@ -105,13 +129,31 @@ public class PlayerPlatform : MonoBehaviour
 
     protected void Flip()
     {
-        isFacingRight = !isFacingRight;
-        transform.Rotate(0.0f, 100.0f, 0.0f);
+        if (!isWallSliding)
+        {
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0.0f, 100.0f, 0.0f);
+        }
+    }
+
+
+    // Draw Area for checking objs(grounding) in Scene(not play)
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
     }
 
     protected void applyMovement()
     {
-        rb2D.velocity = new Vector2(velocity * moveDirection, rb2D.velocity.y);
+        if (isGrounded)
+            rb2D.velocity = new Vector2(velocity * moveDirection, rb2D.velocity.y);
+
+        //Wall Slide session
+        if (isWallSliding)
+        {
+            if (rb2D.velocity.y < -wallSlidingSpeed)
+                rb2D.velocity = new Vector2(rb2D.velocity.x, -wallSlidingSpeed);
+        }
     }
 
     protected void UpdateAnimation()
@@ -120,5 +162,6 @@ public class PlayerPlatform : MonoBehaviour
         anim.SetBool("isGrounded", isGrounded);
         //Seting downward animation : 15:40 in https://www.youtube.com/watch?v=MReoItM8BoI
         anim.SetFloat("yVelocity", rb2D.velocity.y);
+        anim.SetBool("isWallSliding", isWallSliding);
     }
 }
